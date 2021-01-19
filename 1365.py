1060457bd98ed0c7a89fe2718deff00d1b88bef6
Aarush29 <htiwari292001@gmail.com>
2021-01-19 12:02:32 +0530

    Number smaller than current no. solution

---
+++ 1365.py
@@ -0,0 +1,14 @@
+#1365. How Many Numbers Are Smaller Than the Current Number
+
+"""Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+
+Return the answer in an array. """
+
+
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        hm = {}
+        for y, x in enumerate(sorted(nums)):
+            hm.setdefault(x, y)
+
+        return [hm[x] for x in nums]
